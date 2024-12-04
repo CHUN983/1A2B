f3eef04d6c2155fcc004dd782518e3d56b8b4c49
sunthing <apple0908070605@gmail.com>
2024-12-04 22:57:33 +0800

    1a2b questioner version1

--- questioner.c
+++ questioner.c
@@ -42,134 +42,37 @@ void generateRecord(char *record){
     }
 }
 
-void updateRecord(char *record, char *numGuess, int type, int a, int b){
-    for(int i=0; i<10000; i++){
-        int tmp = i;
-        if(record[i]=='O'){
-            int check=0;//for check the before number have been the same
-            if(type==0){
-                for(int j=0; j<4; j++){
-                    int tmpMod = tmp%10;
-                    for(int k=0; k<4; k++){
-                        if(numGuess[k]==tmpMod+'0'){
-                            record[i] = 'X';
-                        }
-                    }
-                    if(record[i]=='X')
-                        break;
-                    tmp/=10;
-                }
-            }else if(type==1 || type == 2){
-                if(a==1 || (a==2&&b==0) ){
-                    for(int j=3; j>=0; j--){
-                        int tmpMod = tmp%10;
-                        for(int k=0; k<4; k++){
-                            if(numGuess[k]==tmpMod+'0'){
-                                if(k!=j){
-                                    record[i] = 'X';
-                                }else if(k==j){
-                                    if(check == a){
-                                        record[i] = 'X';
-                                    }else{
-                                        check ++;
-                                    }
-                                    
-                                }
-                                    
-                            }
-                        }
-                        if(record[i]=='X')
-                            break;
-                        tmp/=10;
-                    }
-                }else if(b==1 || (b==2&&a==0)){
-                    for(int j=3; j>=0; j--){
-                        int tmpMod = tmp%10;
-                        for(int k=0; k<4; k++){
-                            if(numGuess[k]==tmpMod+'0'){
-                                if(k==j){
-                                    record[i] = 'X';
-                                }else if(k!=j){
-                                    if(check == b){
-                                        record[i] = 'X';
-                                    }else{
-                                        check ++;
-                                    }
-                                    
-                                }
-                                    
-                            }
-                        }
-                        if(record[i]=='X')
-                            break;
-                        tmp/=10;
-                    }
-                }else{
 
+void updateRecord(char *record, char *numGuess, int a, int b) {
+    for (int i = 0; i < 10000; i++) {
+        if (record[i] == 'O') {
+            char candidate[5];
+            int temp = i;
+            for (int j = 3; j >= 0; j--) {
+                candidate[j] = temp % 10 + '0';
+                temp /= 10;
+            }
+            candidate[4] = '\0';
+
+            // 計算候選答案的 A 和 B
+            int aCount = 0, bCount = 0;
+            for (int j = 0; j < 4; j++) {
+                if (candidate[j] == numGuess[j]) {
+                    aCount++;
+                } else {
+                    for (int k = 0; k < 4; k++) {
+                        if (candidate[j] == numGuess[k]) {
+                            bCount++;
+                        }
+                    }
                 }
             }
 
-        }
-    }
-}
-
-
-
-void ZeroAB(char *record, char *numGuess){
-
-    updateRecord(record, numGuess, 0, 0 ,0);
-
-    //decide new numGuess
-    for(int i=0; i<10000; i++){
-        if(record[i] == 'O'){
-            for(int j=3; j>=0; j--){
-                numGuess[j] = i%10+'0';
-                i/=10;
+            // 若不符合，將候選答案標記為無效
+            if (aCount != a || bCount != b) {
+                record[i] = 'X';
             }
-            break;
         }
-    }
-
-
-}
-
-void oneAB(char *record, char *numGuess, int a, int b){
-    updateRecord(record, numGuess, a+b, a, b);
-
-    //decide new numGuess
-    for(int i=0; i<10000; i++){
-        if(record[i] == 'O'){
-            for(int j=3; j>=0; j--){
-                numGuess[j] = i%10+'0';
-                i/=10;
-            }
-            break;
-        }
-    }
-}
-
-void twoAB(char* record, char* numGuess, int a, int b){
-    updateRecord(record, numGuess, a+b, a, b);
-}
-
-
-void deleteRecord(char *record, char *numGuess, int a, int b){
-    int sum = a+b;
-    if(sum==0){
-        ZeroAB(record, numGuess);
-    }else if(sum==1){
-        oneAB(record, numGuess, a, b);
-    }else if(sum==2){
-        twoAB(record, numGuess, a, b);
-    }else if(sum==3){
-        //threeAB(record, numGuess, a, b);
-    }else{
-        //fourAB(record, numGuess, a, b);
-    }
-
-    for(int i=0; i<10000; i++){
-        if(record[i]=='O')
-            printf("%d ", i);
     }
 }
 
@@ -190,7 +93,34 @@ int main(){
         printf("Please input _A_B(For example:1A0B->input 1 0)\n");
         scanf("%d %d",&a, &b);
 
-        deleteRecord(record, numGuess, a, b);
+        //the number who isn't 4a should be cannel
+        if(a!=4){
+            int place=0;
+            for(int i=0; i<4; i++){
+                place+=numGuess[i]-'0';
+                place*=10;
+            }
+            place/=10;
+            record[place] = 'X';
+        }
+
+        updateRecord(record, numGuess, a, b);
+
+        for(int i=0; i<10000; i++){
+            if(record[i]=='O'){
+                printf("%d ",i);
+            }
+        }
+
+        for(int i=0; i<10000; i++){
+            if(record[i]=='O'){
+                for(int j=3; j>=0; j--){
+                    numGuess[j] = i%10+'0';
+                    i/=10;
+                }
+                break;
+            }
+        }
         
     }
     
